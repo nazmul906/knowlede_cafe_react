a29d6abb4760d7db06f24c8d986c92c3b329834e
finless88 <nazmul96@gmail.com>
2023-03-31 21:19:55 +0600

    mobile responsiveness is added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Header from "./components/haeder/Header";
+import Header from "./components/header/Header";
 import BlogPost from "./components/blogpost/BlogPost";
 
 function App() {
